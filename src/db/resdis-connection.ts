02b42f0a2351cd1f772ac3f9ebbc06daf17fda8f
Zarar Siddiqi <zarars@gmail.com>
2022-05-11 10:34:53 -0400

    Redis endpoint

--- src/db/resdis-connection.ts
+++ src/db/resdis-connection.ts
@@ -4,8 +4,8 @@ export let redisClient = undefined
 export async function createRedisClient() {
   redisClient = createClient(
     {
-      url: 'redis://redis-17834.c232.us-east-1-2.ec2.cloud.redislabs.com:17834',
-      password: '85wzmmNPEuFnmfLAcLuIQde7LXFGZRh2'
+      url: 'redis://redis-19886.c9.us-east-1-2.ec2.cloud.redislabs.com:19886',
+      password: 'BlyGohE1AtxntXTZjLjZN4BZ67ysfMvx'
     }
   );
 
